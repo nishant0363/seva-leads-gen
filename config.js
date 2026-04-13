448a7adb0663b20da48f8d818959db81322cdcba
nishant0363 <nishant0363@gmail.com>
2026-04-13 16:37:04 +0530

    new

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwitQ7jM3aDoReK7tmZzOqz73vb67PTveN2pD2v658YwUrQVYlrdWp_wUNkbr-wbxSuAA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxENuKpIA32ZwPFEAuRKSPk6Z_0ZlV3U1H5Bj2x8ydbHgtksOF5ChfgKjCuXTatourxQw/exec",
   GOOGLE_API_KEY: ""
 };
